b867b771dd1288353f0c02eee8913e4bd975c7be
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2024-02-21 00:00:56 +0500

    test

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>salom</h1>
+    <h1>salom</h1>
+    <h1>salom</h1>
   </React.StrictMode>
 )
